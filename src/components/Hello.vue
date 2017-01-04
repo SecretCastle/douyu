0458139a0021553b419e07e75fb44d25dd258470
SecretCastle <chenfangzhong@hotmail.com>
2017-01-04 23:45:36 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <div>{{ msg }}</div>
+    <div class="">
+        {{roomlist}}
+    </div>
+  </div>
+</template>
+
+<script>
+const reqURL = "http://open.douyucdn.cn/api/RoomApi/live";
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: "hello world",
+      roomlist : {}
+    }
+  },
+  created(){
+      this.$http.get("/live",{
+          method : "GET",
+          credentials : true
+      }).then(function(res){
+          if(res){
+              if(typeof res === "string"){
+                  res = JSON.parse(res);
+              }
+          }
+          this.$set(this,"roomlist",res.data.data);
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
