fceb52f922f17f9cdb5942743232f6762bfd9fb6
SecretCastle <chenfangzhong@hotmail.com>
2017-01-06 13:55:37 +0800

    import aui2.0

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
-    <router-link to="/home">分类</router-link>
-    <router-link to="/list">全部</router-link>
+      <div class="aui-content">
+          <div class="aui-tab" id="tab">
+              <router-link class="aui-tab-item" to="/list">全部</router-link>
+              <router-link class="aui-tab-item" to="/home">分类</router-link>
+          </div>
+      </div>
+
     <router-view></router-view>
   </div>
 </template>
-
-<script>
-
-</script>
-
 <style>
+@import './assets/aui.2.0.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+.router-link-active{
+    border-bottom: 2px solid #999;
 }
 </style>
